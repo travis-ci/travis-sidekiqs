108fc4bdb98839f1a531d5bb859d1893f39eb04d
Mathias Meyer <meyer@paperplanes.de>
2012-11-01 14:20:44 +0100

    Add namespace to blackhole.

--- lib/travis/sidekiq/blackhole.rb
+++ lib/travis/sidekiq/blackhole.rb
@@ -1,10 +1,14 @@
 require 'sidekiq/worker'
 
-class Blackhole
-  include Sidekiq::Worker
+module Travis
+  module Sidekiq
+    class Blackhole
+      include Sidekiq::Worker
 
-  def perform(message)
-    puts message
-    raise
+      def perform(message)
+        puts message
+        raise
+      end
+    end
   end
 end
