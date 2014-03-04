2ef1b1cd5466784c3af15a23fc9aee42426b6ab2
Henrik Hodne <henrik@hodne.io>
2014-03-04 03:15:13 -0600

    feat(sync-user): let the service know if the sync was forced or not

--- lib/travis/sidekiq/synchronize_user.rb
+++ lib/travis/sidekiq/synchronize_user.rb
@@ -7,7 +7,7 @@ module Travis
       sidekiq_options retry: 5, queue: :user_sync
 
       def perform(user_id)
-        Travis.run_service(:github_sync_user, id: user_id)
+        Travis.run_service(:github_sync_user, id: user_id, force: true)
       end
     end
   end
